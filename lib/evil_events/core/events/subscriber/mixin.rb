7140b9554c7e6a25b413b04ef932f9da01a17c6d
Rustam Ibragimov <iamdaiver@icloud.com>
2017-11-16 03:16:57 +0300

    Ability to subscribe to the event list

--- lib/evil_events/core/events/subscriber/mixin.rb
+++ lib/evil_events/core/events/subscriber/mixin.rb
@@ -4,21 +4,25 @@ class EvilEvents::Core::Events::Subscriber
   # @api public
   # @since 0.1.0
   Mixin = EvilEvents::Shared::ClonableModuleBuilder.build do
-    # @param event_type [String, Class{EvilEvents::Core::Events::AbstractEvent}]
+    # @param event_type [Array<String, Class{EvilEvents::Core::Events::AbstractEvent}, Regexp>]
     # @param delegator [String, Symbol, NilClass]
-    # @raise [IncompatibleEventAttrTypeError]
+    # @raise [ArgumentError]
     #
-    # @since 0.1.0
-    def subscribe_to(event_type, delegator: nil)
-      case event_type
-      when Class
-        EvilEvents::Core::Bootstrap[:event_system].observe(event_type, self, delegator)
-      when String
-        EvilEvents::Core::Bootstrap[:event_system].raw_observe(event_type, self, delegator)
-      when Regexp
-        EvilEvents::Core::Bootstrap[:event_system].observe_list(event_type, self, delegator)
-      else
-        raise ArgumentError
+    # @since 0.2.0
+    def subscribe_to(*event_types, delegator: nil)
+      raise ArgumentError unless event_types.all? do |event_type|
+        event_type.is_a?(Class) || event_type.is_a?(String) || event_type.is_a?(Regexp)
+      end
+
+      event_types.each do |event_type|
+        case event_type
+        when Class
+          EvilEvents::Core::Bootstrap[:event_system].observe(event_type, self, delegator)
+        when String
+          EvilEvents::Core::Bootstrap[:event_system].raw_observe(event_type, self, delegator)
+        when Regexp
+          EvilEvents::Core::Bootstrap[:event_system].observe_list(event_type, self, delegator)
+        end
       end
     end
   end
